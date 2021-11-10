e4920b8957cfa34b3d7e6acab960800ee12500ff
emanuel <emanuelzm@protonmail.com>
2021-11-10 16:01:53 -0300

    fix/ fix types files, add more files

--- src/providers/Questions/index.tsx
+++ src/providers/Questions/index.tsx
@@ -1,12 +1,14 @@
-import { createContext, useState, useEffect, useContext } from "react";
-import { ChildrenProps } from "../../types";
+import { createContext, useState, useEffect, useContext } from 'react';
+import { ChildrenProps } from '../../types/children';
 
 export const QuestionsContext = createContext({});
 
 export const QuestionsProvider = ({ children }: ChildrenProps) => {
-  return (
-    <QuestionsContext.Provider value={{}}>{children}</QuestionsContext.Provider>
-  );
+    return (
+        <QuestionsContext.Provider value={{}}>
+            {children}
+        </QuestionsContext.Provider>
+    );
 };
 
 export const useQuestions = () => useContext(QuestionsContext);
--- src/providers/User/index.tsx
+++ src/providers/User/index.tsx
@@ -1,10 +1,10 @@
-import { createContext, useState, useEffect, useContext } from "react";
-import { ChildrenProps } from "../../types";
+import { createContext, useState, useEffect, useContext } from 'react';
+import { ChildrenProps } from '../../types/children';
 
 export const UserContext = createContext({});
 
 export const UserProvider = ({ children }: ChildrenProps) => {
-  return <UserContext.Provider value={{}}>{children}</UserContext.Provider>;
+    return <UserContext.Provider value={{}}>{children}</UserContext.Provider>;
 };
 
 export const useUsers = () => useContext(UserContext);
