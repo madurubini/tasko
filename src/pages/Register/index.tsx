2311235711a70f690cde81261f0d04163d006dda
emanuel <emanuelzm@protonmail.com>
2021-11-11 12:50:56 -0300

    fix/ revert "fix/ fix errors style bug"
    This reverts commit a732acef7edeb485e70d9098c148d67dfa67e8be.

--- src/pages/Register/index.tsx
+++ src/pages/Register/index.tsx
@@ -1,27 +1,19 @@
-import { motion } from 'framer-motion';
 import { RegisterCard } from '../../components/RegisterCard';
 import clipArt from './../../assets/image/GreenMan.png';
 import { CardBox, ClipArtBox, MainContainer } from './style';
 
 const Register = () => {
     return (
-        <motion.div
-            layout
-            initial={{ opacity: 0 }}
-            animate={{ opacity: 1 }}
-            exit={{ opacity: 0 }}
-        >
-            <MainContainer>
-                <CardBox>
-                    <RegisterCard />
-                </CardBox>
-                <ClipArtBox>
-                    <picture>
-                        <img src={clipArt} alt="" />
-                    </picture>
-                </ClipArtBox>
-            </MainContainer>
-        </motion.div>
+        <MainContainer>
+            <CardBox>
+                <RegisterCard />
+            </CardBox>
+            <ClipArtBox>
+                <picture>
+                    <img src={clipArt} alt="" />
+                </picture>
+            </ClipArtBox>
+        </MainContainer>
     );
 };
 
