20a6abe6e9631d72a89e89bbd918e033259188eb
Julia Gama <gamaj235@gmail.com>
2021-11-10 23:12:28 -0300

    feat/page login in progress

--- src/pages/Login/index.tsx
+++ src/pages/Login/index.tsx
@@ -1,5 +1,24 @@
+import { Flex,Text,Heading,Image } from "@chakra-ui/react";
 const Login = () => {
-  return <>Login</>;
+  return <>
+    <Flex 
+    height="100vh"
+    width="100%"
+    justifyContent="flex-start"
+    alignItems="center"
+    background="var(--background)"
+    >
+    
+    <Flex flexDirection="column" alignItems="center"
+    justifyContent="center"
+    width="100%"
+    maxWidth="700px">
+      <Heading fontWeight="400" color="var(--font-color)" fontSize="48px">Entre com a sua conta</Heading>
+      <Text color="var(--font-color)" fontSize="24px">Planeje o seu dia!</Text>
+    </Flex>
+
+    </Flex>
+  </>;
 };
 
 export default Login;
--- src/styles/GlobalStyles.ts
+++ src/styles/GlobalStyles.ts
@@ -2,11 +2,12 @@ import { createGlobalStyle } from "styled-components";
 
 export default createGlobalStyle`
   *{    
-      margin: 0;
+        margin: 0;
         padding: 0;
         box-sizing: border-box;
         outline: 0;
         list-style-type: none;
+        text-align: center;
 }
 
 body {
