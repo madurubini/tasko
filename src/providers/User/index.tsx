013df381c6f37ef4ffcbfe4bc617e15c98200126
Willian Santos <williansantosd2@gmail.com>
2021-11-12 15:45:55 -0300

    feat/ menu dashboard desktop e created

--- src/providers/User/index.tsx
+++ src/providers/User/index.tsx
@@ -24,7 +24,7 @@ export const UserProvider = ({ children }: UserProps) => {
                 localStorage.setItem('@token', data.accessToken);
                 localStorage.setItem('@id', data.user.id);
                 setId(data.user.id);
-                setUserName(data.user.name);
+                setUserName(data.user.firstName);
             })
             .catch((error) => console.error('Miss', error));
     };
