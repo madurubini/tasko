b8ca535e25edb400fe5ea99f3b5011511e768e6b
Lucas Elias <lucaselias2509@gmail.com>
2021-11-15 19:20:42 -0300

    feat/ level update according to xp

--- src/components/MenuDesktop/index.tsx
+++ src/components/MenuDesktop/index.tsx
@@ -2,15 +2,16 @@ import { Container, MenuButton } from './styles';
 import User from '../../assets/image/user.png';
 import { VStack } from '@chakra-ui/layout';
 import { useHistory } from 'react-router';
-
+import { useLevels } from '../../providers/Levels';
 const MenuDesktop = () => {
     const history = useHistory();
+    const { userLevel } = useLevels();
     return (
         <Container>
             <div>
                 <img src={User} alt="" />
                 <h3>Usuário</h3>
-                <p>Level: 0000</p>
+                <p>Level: {userLevel.LevelId}</p>
             </div>
             <VStack spacing="5">
                 <MenuButton onClick={() => history.push('/comunidade')}>
