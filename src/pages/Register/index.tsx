2a661c8518ed38cfaacad009dd6dfdf92841cad5
Emanuel <emanuelzm@protonmail.com>
2021-11-17 07:58:08 -0300

    style/ transition on animations at register page added

--- src/pages/Register/index.tsx
+++ src/pages/Register/index.tsx
@@ -14,6 +14,7 @@ const Register = () => {
                 initial={{ opacity: 0 }}
                 animate={{ opacity: 1 }}
                 exit={{ opacity: 0 }}
+                transition={{ duration: 2 }}
             >
                 <LogoHeader>
                     {' '}
